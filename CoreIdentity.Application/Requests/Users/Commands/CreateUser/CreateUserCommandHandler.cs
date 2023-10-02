7fe125393a3fbac7e089f8f6aaf0758e30fdfbd1
Kenneth Almario <kenzalmario@gmail.com>
2023-10-02 10:37:49 +0800

    additional checking for existing user

--- CoreIdentity.Application/Requests/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ CoreIdentity.Application/Requests/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using CoreIdentity.Application.Common.Interfaces;
+using CoreIdentity.Application.Requests.Tenants.Commands.AddUsers;
+using CoreIdentity.Application.Requests.Users.Commands.AddUserRole;
 using CoreIdentity.Application.Requests.Users.Queries;
 using CoreIdentity.Domain.Entity;
 using MediatR;
@@ -11,33 +13,33 @@ namespace CoreIdentity.Application.Requests.Users.Commands;
 public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserDto>
 {
     private readonly ICoreIdentityDbContext _dbContext;
+    private readonly IMediator _mediator;
     private readonly IMapper _mapper;
 
-    public CreateUserCommandHandler(ICoreIdentityDbContext dbContext, IMapper mapper)
+    public CreateUserCommandHandler(ICoreIdentityDbContext dbContext, IMapper mapper, IMediator mediator)
     {
         _dbContext = dbContext;
         _mapper = mapper;
+        _mediator = mediator;
     }
 
     public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        var userExist = await _dbContext.Users
-            .Where(o => o.UserName == request.UserName || o.MobileNumber == request.MobileNumber)
-            .AnyAsync(cancellationToken);
-        
-        if (userExist)
-            throw new Exception($"UserName or Mobile Number is already exist!");
-
         var tenant = await _dbContext.Tenants.Where(o => o.Id == request.TenantId)
             .SingleOrDefaultAsync(cancellationToken);
 
         _ = tenant ?? throw new Exception($"Unable to find Tenant with TenantId {request.TenantId}");
 
+        var existingUserCheck = await TryCheckExistingUser(request, cancellationToken);
+
+        if (existingUserCheck.IsExitingUser)
+            return existingUserCheck.User;
+
         var role = await _dbContext.Roles.Where(o => o.Id == request.RoleId)
             .SingleOrDefaultAsync(cancellationToken);
 
         _ = tenant ?? throw new Exception($"Unable to find Role with RoleId {request.RoleId}");
-        
+
         var password = request.Password;
 
         if (string.IsNullOrWhiteSpace(password))
@@ -54,12 +56,12 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserD
             MobileNumber = request.MobileNumber,
             Password = passwordHash.Password,
             PasswordSalt = passwordHash.Salt,
-            UserRoles = new [] { new UserRoles
+            UserRoles = new[] { new UserRoles
                 {
                     RoleId = role.Id
                 }
             },
-            TenantUsers = new [] {
+            TenantUsers = new[] {
                 new TenantUser {
                     TenantId = tenant.Id
                 }
@@ -70,5 +72,39 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserD
         await _dbContext.SaveChangesAsync(cancellationToken);
 
         return _mapper.Map<UserDto>(user);
+    }
+
+    private async Task<(bool IsExitingUser, UserDto User)> TryCheckExistingUser(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var existingUser = await _dbContext.Users
+            .Include(o => o.TenantUsers)
+            .Include(o => o.UserRoles)
+            .Where(o => o.UserName == request.UserName || o.MobileNumber == request.MobileNumber)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (existingUser == null)
+            return (false, null);
+
+        try
+        {
+            // if user is already exist, should return the current user and add the tenant and role
+            if (!existingUser.TenantUsers.Select(o => o.TenantId).Contains(request.TenantId))
+            {
+                var tenantRequest = new AddUsersCommand(request.TenantId, new[] { existingUser.Id });
+                await _mediator.Send(tenantRequest, cancellationToken);
+            }
+
+            if (!existingUser.UserRoles.Select(o => o.RoleId).Contains(request.RoleId))
+            {
+                var roleRequest = new AddUserRoleCommand(existingUser.Id, request.RoleId);
+                await _mediator.Send(roleRequest, cancellationToken);
+            }
+
+            return (true, _mapper.Map<UserDto>(existingUser));
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Unable to update User {request.MobileNumber} because of this error!", ex);
+        }
     }
 }
