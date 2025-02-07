8c74a9ae0df1fafb680d5ab1727902da6c954545
Krishia <krishiaranquebello@gmail.com>
2025-02-07 17:49:56 +0800

    refactor get locked users endpoint to cater null pagination

--- CoreIdentity.Application/Requests/Users/Queries/GetLockedUsers/GetLockedUsersQuery.cs
+++ CoreIdentity.Application/Requests/Users/Queries/GetLockedUsers/GetLockedUsersQuery.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CoreIdentity.Application.Requests.Users.Queries.GetLockedUsers
 {
-    public record GetLockedUsersQuery(Guid CompanyObjectId, int PageNumber, int PageSize) : IRequest<LockedUserVm>;
+    public record GetLockedUsersQuery(Guid CompanyObjectId, int? PageNumber = null, int? PageSize = null) : IRequest<LockedUserVm>;
     public class GetLockedUsersQueryHandler(ICoreIdentityDbContext dbContext, IMapper mapper) : IRequestHandler<GetLockedUsersQuery, LockedUserVm>
     {
         private readonly ICoreIdentityDbContext _dbContext = dbContext;
@@ -20,10 +20,11 @@ namespace CoreIdentity.Application.Requests.Users.Queries.GetLockedUsers
 
             var total = await query.CountAsync();
 
-            if (request.PageNumber > 1)
-                query = query.Skip((request.PageNumber - 1) * request.PageSize);
-
-            query = query.Take(request.PageSize);
+            if (request.PageNumber.HasValue && request.PageSize.HasValue)
+            {
+                query = query.Skip((request.PageNumber.Value - 1) * request.PageSize.Value)
+                              .Take(request.PageSize.Value);
+            }
 
             var userslist = await query
                     .ProjectTo<LockedUserDto>(_mapper.ConfigurationProvider)
@@ -33,8 +34,8 @@ namespace CoreIdentity.Application.Requests.Users.Queries.GetLockedUsers
             {
                 Results = userslist,
                 Total = total,
-                PageNumber = request.PageNumber,
-                PageSize = request.PageSize
+                PageNumber = request.PageNumber ?? 1,
+                PageSize = request.PageSize ?? total
             };
         }
     }
