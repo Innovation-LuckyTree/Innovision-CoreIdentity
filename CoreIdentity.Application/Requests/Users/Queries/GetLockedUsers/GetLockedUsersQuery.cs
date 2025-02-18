f65f1f5faef119adbca4ee14d0a6af381636dd84
kenzalmario@gmail.com <kenzalmario@gmail.com>
2025-02-18 18:04:11 +0800

    small fixes for get locked users query

--- CoreIdentity.Application/Requests/Users/Queries/GetLockedUsers/GetLockedUsersQuery.cs
+++ CoreIdentity.Application/Requests/Users/Queries/GetLockedUsers/GetLockedUsersQuery.cs
@@ -1,42 +1,5 @@
-﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using CoreIdentity.Application.Common.Interfaces;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
 
-namespace CoreIdentity.Application.Requests.Users.Queries.GetLockedUsers
-{
-    public record GetLockedUsersQuery(Guid CompanyObjectId, int? PageNumber = null, int? PageSize = null) : IRequest<LockedUserVm>;
-    public class GetLockedUsersQueryHandler(ICoreIdentityDbContext dbContext, IMapper mapper) : IRequestHandler<GetLockedUsersQuery, LockedUserVm>
-    {
-        private readonly ICoreIdentityDbContext _dbContext = dbContext;
-        private readonly IMapper _mapper = mapper;
+namespace CoreIdentity.Application.Requests.Users.Queries.GetLockedUsers;
 
-        public async Task<LockedUserVm> Handle(GetLockedUsersQuery request, CancellationToken cancellationToken)
-        {
-            var query = _dbContext.Users.Where(o => o.CompanyId == request.CompanyObjectId && o.Locked)
-                    .OrderByDescending(m => m.LockTime)
-                    .AsQueryable();
-
-            var total = await query.CountAsync();
-
-            if (request.PageNumber > 1)
-                query = query.Skip((request.PageNumber - 1) * request.PageSize);
-
-            if (request.PageSize > 0)
-                query = query.Take(request.PageSize);
-
-            var userslist = await query
-                    .ProjectTo<LockedUserDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
-
-            return new LockedUserVm
-            {
-                Results = userslist,
-                Total = total,
-                PageNumber = request.PageNumber ?? 1,
-                PageSize = request.PageSize ?? total
-            };
-        }
-    }
-}
+public record GetLockedUsersQuery(Guid CompanyObjectId, int? PageNumber = null, int? PageSize = null) : IRequest<LockedUserVm>;
